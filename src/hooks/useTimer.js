72c46cf81c5d35f6c47e8e5ef3dbdfb502a8d5f7
Erik Z <molagg92@gmail.com>
2024-01-04 14:56:43 -0800

    fill out useTimer useEffect method

--- src/hooks/useTimer.js
+++ src/hooks/useTimer.js
@@ -1,7 +1,20 @@
 import { useState, useEffect } from 'react';
 
 function useTimer() {
-  // stateful logic for our timer will go here!
+  const [isActive, setIsActive] = useState(false);
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    let interval;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setTimer(timerState => timerState + 1)
+      }, 1000
+    )}
+
+    return () => clearInterval(interval);
+  }, [isActive]);
 }
 
 export default useTimer;
